14326fa50c9069180c309ec33961fd7ccb2202c5
kombatross <konradnater1@wp.pl>
2021-03-10 16:31:32 +0100

    jej

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ fetch('https://jsonplaceholder.typicode.com/posts/'+i)
 
   var post =fetch('https://jsonplaceholder.typicode.com/posts/1') .then((response) => response.json());
 
-  var comment =fetch('https://jsonplaceholder.typicode.com/posts/1/comments') .then((response) => response.json());
 
 
 
 post.then(value => {document.getElementById("PostPlace").innerHTML = "Id uzytkownika:<br> " +value.userId +"<br><br> tytuł: <br> " + value.title + "<br><br>zawartość: <br> " + value.body});
   
-comment.then(value => {document.getElementById("CommentPlace").innerHTML = "Nazwa: " + value.name + "<br> Email: " + value.email + "<br> Treść Komentarza: "+ value.body})
